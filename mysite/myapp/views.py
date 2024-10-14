78ac2cfe6c4503c59746fe6d837b7dd732b11930
22uit059 <166804341+22uit059@users.noreply.github.com>
2024-10-14 12:40:15 +0530

    Add files via upload

---
+++ mysite/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context={}
+    return render(request, 'myapp/home.html',context)
+
+def about(request):
+    return render(request, 'myapp/about.html')
